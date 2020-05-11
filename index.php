b329c2ae1f0f3678b29b58c96d784095476e413f
Tufayel Ahmed <39314838+TufayelLUS@users.noreply.github.com>
2020-05-11 22:22:03 +0600

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ function getContent($url)
     'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Encoding: gzip, deflate, br',
     'Accept-Language: en-US,en;q=0.9',
-    'Range: bytes=0-20000'
+    'Range: bytes=0-200000'
 	];
 
     $options = array(
